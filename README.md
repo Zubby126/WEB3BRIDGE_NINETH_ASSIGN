7d99501c881b8fdae85836bdd9eb4f08803f38f8
Ebubedike Nzube <zubby126@gmail.com>
2024-10-07 23:05:39 +0100

    my nineth we3bridge commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WEB3BRIDGE_NINETH_ASSIGN
+
+Creation of Simple Arithmatic using JavaScript
---
+++ index.js
@@ -0,0 +1,69 @@
+alert('This is a simple arithematic by Ebubedike Nzube Edwin')
+
+document.write('Building a simple calculator using Javascript<br>');
+document.write('<br>')
+document.write('<br>')
+document.write('<br>')
+
+
+// Addition
+var boys = 10;
+let girls = 5;
+const pupils = boys + girls;
+
+document.write('The students are total of: ' + pupils)
+document.write('<br>')
+document.write('<br>')
+
+
+// Substraction
+const hour = 5;
+var minutes = 4.5;
+let period = hour - minutes;
+
+document.write('The days spent on the road is ' + period)
+document.write('<br>')
+document.write('<br>')
+
+
+// Division
+var state = 36;
+var city = 6;
+var town = state / city;
+
+document.write('town = ' + town)
+document.write('<br>')
+document.write('<br>')
+
+// Multiplication
+const withdrawal = 20;
+const deposit = 50;
+let balance = deposit * withdrawal;
+
+document.write('balance ' + balance)
+document.write('<br>')
+document.write('<br>')
+
+
+// modulo operation
+const float = 3;
+const interger = 17;
+let answer = float % interger;
+
+document.write('answer ' + answer)
+document.write('<br>')
+document.write('<br>')
+
+// Raise to power
+
+let denominator = 3;
+let numerator = 5;
+let respond = denominator ** numerator;
+
+document.write('respond ' + respond)
+
+
+
+
+
+
